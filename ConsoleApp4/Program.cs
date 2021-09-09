8ea8cbef99d5e813038b2cd9d36554d758c74465
Danila <karantinz@mail.ru>
2021-09-09 15:05:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictures;
+            int totalPictures = 52;
+            int filledRows = 0;
+
+            for (pictures = 0; pictures < totalPictures; pictures += 3)
+            {
+                Console.WriteLine("Картинок - " + pictures);
+                Console.WriteLine("Всего рядов заполнено - " + filledRows++);
+                Console.ReadKey();
+            }
+        }
+    }
+}
